7c692961f6bd1fbab8580c45766efc22f2b0dd8d
Morgan Mora Martínez <morganmora@gmail.com>
2018-10-10 23:14:56 +0200

    neural class definition

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 import numpy as np
-
-# X = (hours studying, hours sleeping), y = score on test, xPredicted = 4 hours studying & 8 hours sleeping (input data for prediction)
-#X = np.array(([2, 9], [1, 5], [3, 6]), dtype=float)
-#y = np.array(([92], [86], [89]), dtype=float)
-#xPredicted = np.array(([4,8]), dtype=float)
-
-# scale units
-#X = X/np.amax(X, axis=0) # maximum of X array
-#xPredicted = xPredicted/np.amax(xPredicted, axis=0) # maximum of xPredicted (our input data for the prediction)
-#y = y/100 # max test score is 100
+import neural
 
 class Key(object):
     """
@@ -44,72 +35,6 @@ class Key(object):
 
 
 
-class Neural_Network(object):
-  def __init__(self):
-  #parameters
-    self.inputSize = 5
-    self.outputSize = 5
-    self.hiddenSize = 25
-
-  #weights
-    self.W1 = np.random.randn(self.inputSize, self.hiddenSize) # (3x2) weight matrix from input to hidden layer
-    self.W2 = np.random.randn(self.hiddenSize, self.outputSize) # (3x1) weight matrix from hidden to output layer
-
-  def forward(self, X):
-    #forward propagation through our network
-    self.z = np.dot(X, self.W1) # dot product of X (input) and first set of 3x2 weights
-    self.z2 = self.sigmoid(self.z) # activation function
-    self.z3 = np.dot(self.z2, self.W2) # dot product of hidden layer (z2) and second set of 3x1 weights
-    o = self.sigmoid(self.z3) # final activation function
-    return o
-
-  def sigmoid(self, s):
-    # activation function
-    return 1/(1+np.exp(-s))
-
-  def sigmoidPrime(self, s):
-    #derivative of sigmoid
-    return s * (1 - s)
-
-  def backward(self, X, y, o):
-    # backward propagate through the network
-    self.o_error = y - o # error in output
-    self.o_delta = self.o_error*self.sigmoidPrime(o) # applying derivative of sigmoid to error
-
-    self.z2_error = self.o_delta.dot(self.W2.T) # z2 error: how much our hidden layer weights contributed to output error
-    self.z2_delta = self.z2_error*self.sigmoidPrime(self.z2) # applying derivative of sigmoid to z2 error
-
-    self.W1 += X.T.dot(self.z2_delta) # adjusting first set (input --> hidden) weights
-    self.W2 += self.z2.T.dot(self.o_delta) # adjusting second set (hidden --> output) weights
-
-  def train(self, X, y):
-    o = self.forward(X)
-    self.backward(X, y, o)
-
-  def saveWeights(self):
-    np.savetxt("w1.txt", self.W1, fmt="%s")
-    np.savetxt("w2.txt", self.W2, fmt="%s")
-
-  def predict(self):
-    print ("Predicted data based on trained weights: ")
-    print ("Input (scaled): \n" + str(xPredicted))
-    print ("Output: \n" + str(self.forward(xPredicted)))
-
-"""
-k = Key([6,15,20,30,38])
-
-X = np.array(k.get_binary_representation(), dtype=bool)
-
-k = Key([7,17,29,37,45])
-
-y = np.array(k.get_binary_representation(), dtype=bool)
-
-k = Key([8,16,24,26,35])
-
-xPredicted = np.array(k.get_binary_representation(), dtype=bool)
-"""
-
-
 def print_representation(key):
     k = Key(key)
 
@@ -126,19 +51,24 @@ find value in the blackbox, the refine with more componets, kind of reverse-engi
 
 """
 
-
+#3, 12, 36, 44, 45 
 #8, 16, 24, 26, 35
 #7, 17, 29, 37, 45
 #2, 4, 8, 27, 50
 #6, 15, 20, 30, 38
 #5, 7, 21, 25, 37
 #3, 8, 10, 32, 45
-
+#1, 3, 33, 40, 45
+#9 18 32 38 46
 
 #X = (hours studying, hours sleeping), y = score on test, xPredicted = 4 hours studying & 8 hours sleeping (input data for prediction)
-X = np.array(([3, 8, 10, 32, 45], [5, 7, 21, 25, 37], [6, 15, 20, 30, 38], [2, 4, 8, 27, 50]), dtype=int)
-y = np.array(( [5, 7, 21, 25, 37], [6, 15, 20, 30, 38], [2, 4, 8, 27, 50], [7, 17, 29, 37, 45]), dtype=int)
-xPredicted = np.array(([7, 17, 29, 37, 45]), dtype=int)
+#X = np.array(([3, 8, 10, 32, 45], [5, 7, 21, 25, 37], [6, 15, 20, 30, 38], [2, 4, 8, 27, 50],[8, 16, 24, 26, 35]), dtype=int)
+#y = np.array(( [5, 7, 21, 25, 37], [6, 15, 20, 30, 38], [2, 4, 8, 27, 50], [7, 17, 29, 37, 45],[8, 16, 24, 26, 35]), dtype=int)
+#xPredicted = np.array(([7, 17, 29, 37, 45]), dtype=int)
+
+X = np.array(([3, 8, 10, 32, 45],[6, 15, 20, 30, 38],[7, 17, 29, 37, 45],[9, 18, 32, 38, 46]), dtype=int)
+y = np.array(([5, 7, 21, 25, 37], [2, 4, 8, 27, 50], [8, 16, 24, 26, 35],[1, 3, 33, 40, 45]), dtype=int)
+xPredicted = np.array(([3, 12, 36, 44, 45]), dtype=int)
 
 # scale units
 X = X/50 # maximum of X array
@@ -146,8 +76,8 @@ xPredicted = xPredicted/50 # maximum of xPredicted (our input data for the predi
 y = y/50 # max test score is 100
 
 
-NN = Neural_Network()
-for i in range(1000): # trains the NN 1,000 times
+NN = neural.Network(5,5,25)
+for i in range(10000): # trains the NN 1,000 times
   #print ("# " + str(i) + "\n")
   #print ("Input (scaled): \n" + str(X))
   #print ("Actual Output: \n" + str(y))
@@ -158,7 +88,7 @@ for i in range(1000): # trains the NN 1,000 times
 
 
 NN.saveWeights()
-NN.predict()
+NN.predict(xPredicted)
 
 
 def get_fingerprint(key = []):
@@ -185,23 +115,19 @@ def get_binary_representation(key = []):
 
 
 ###Test number 2 with binary arrays
-X = np.array(get_binary_representation([3, 8, 10, 32, 45]),
-get_binary_representation([5, 7, 21, 25, 37]),
+X = np.array((get_binary_representation([3, 8, 10, 32, 45]),
 get_binary_representation([6, 15, 20, 30, 38]),
-get_binary_representation([2, 4, 8, 27, 50]), 
-dtype=int)
+get_binary_representation([7, 17, 29, 37, 45])), dtype=int)
 
-y = np.array(get_binary_representation([5, 7, 21, 25, 37]), 
-get_binary_representation([6, 15, 20, 30, 38]), 
-get_binary_representation([2, 4, 8, 27, 50]), 
-get_binary_representation([7, 17, 29, 37, 45]), 
-dtype=int)
+y = np.array((get_binary_representation([5, 7, 21, 25, 37]), 
+get_binary_representation([2, 4, 8, 27, 50]),
+get_binary_representation([8, 16, 24, 26, 35])), dtype=int)
 
-xPredicted = np.array(get_binary_representation([7, 17, 29, 37, 45]), dtype=int)
+xPredicted = np.array(get_binary_representation([3, 12, 36, 44, 45]), dtype=int)
 
 
-N2 = Neural_Network()
-for i in range(1000): # trains the NN 1,000 times
+N2 = neural.Network(51,51,2601)
+for i in range(10000): # trains the NN 1,000 times
   #print ("# " + str(i) + "\n")
   #print ("Input (scaled): \n" + str(X))
   #print ("Actual Output: \n" + str(y))
@@ -212,5 +138,8 @@ for i in range(1000): # trains the NN 1,000 times
 
 
 N2.saveWeights()
-N2.predict()
+N2.predict(xPredicted)
+
+
+
 
